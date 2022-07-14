3ac13e566bb0018e278030e377fc1ef2664dfe70
gerrygralton <gerry@gralton.net>
2022-07-14 16:30:43 +0800

    Add simpsons and trapezoid

---
+++ numerical_integration/lectures/simpsons.py
@@ -0,0 +1,25 @@
+# Author: Gerry Gralton
+# E-mail: gerry.gralton@uwa.edu.au
+# Date: 30/06/2022
+
+
+def simpsons(f_name: str, a: int, b: int, k: int)
+# simpsons() will integrate the function f_name over the interval
+#   a<x<b using n = 2^k panels and return the intergral.
+#   The use is:
+#        s = simpsons(f_name,a,b,k)
+# ==========================================================
+    if k<=0:
+        error("k must be a positive integer")
+# ==========================================================
+    n = 2**k # Number of partitions
+    h = (b - a) / n
+
+    for i in range(1, n):
+        x_i = a + i * h
+        if (i % 2 == 0):
+            evensum += f_name(x_i)
+        else:
+            oddsum += f_name(x_i)
+
+    return h/3 * (f_name(a) + 4 * oddsum + 2 * evensum + f_name(b))
---
+++ numerical_integration/lectures/trapezoid.py
@@ -0,0 +1,26 @@
+# Author: Gerry Gralton
+# E-mail: gerry.gralton@uwa.edu.au
+# Date: 30/06/2022
+
+def trapezoid(f_name: str, a: int, b: int, n: int)
+
+    # trapezoid() integrates function f_name in the interval [a b]
+    # using trapezoidal rule (on n intervals) and
+    # illustrates graphically the procedure.
+    # The function is called as:
+    # trapezoid(f_name,a,b,n)
+
+    h = (b - a) / n     # Width of each partition
+
+    midsum = 0          # Initialise values for use with looping
+
+    # Now we need to calculate the function values at the interior points
+    # We can do that with a `for` loop
+    # Don't forget that range() doesn't include the last number
+
+    for i in range(1, n):
+        x_i = a + i * h
+        midsum += f_name(x_i)
+
+    # Compute the integral:
+    return h/2 * (f_name(a) + 2 * midsum + f_name(b))
